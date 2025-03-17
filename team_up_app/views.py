491c6717e833bb59ee4b8d4e411b8b07b5dce056
mel-th <meleyth@gmail.com>
2025-03-17 00:45:32 -0400

    @ MVP! need to add styling and API integration

--- team_up_app/views.py
+++ team_up_app/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from .models import Team
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
@@ -7,27 +8,11 @@ from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required # function based views
-from django.contrib.auth.mixins import LoginRequiredMixin # for CBVS
+from django.contrib.auth.decorators import login_required  # function based views
+from django.contrib.auth.mixins import LoginRequiredMixin  # for CBVS
 
 
 # Import HttpResponse to send text-based responses
-""" class Team():
-    def __init__(self,name,mid,top,bot,sup,jg):
-        self.name = name
-        self.mid = mid
-        self.top = top]
-        self.bot = bot
-        self.jg = jg
-        self.sup = sup
- 
-teams = [
-    Team('winners', 'middle', 'top', 'bottom', 'jungle', 'soraka'),
-    Team('jinxers', 'fizz', 'trundle', 'caitlyn', 'rengar', 'nami'),
-    Team('topsiders', 'neeko', 'fiora', 'jinx', 'warwick', 'biltzcrank'),
-    Team('rats', 'katarina', 'kled', 'twitch', 'teemo', 'pantheon')
-]
-"""
 
 # Define the home view function
 
@@ -36,59 +21,87 @@ def home(request):
     # Send a simple HTML response
     return HttpResponse('<h1>Home sweet home</h1>') """
 
+
 def about(request):
-    return render(request, 'about.html')
-    #return HttpResponse('<h1>about</h1>')
+    return render(request, "about.html")
+    # return HttpResponse('<h1>about</h1>')
+
 
 """ def index(request):
     teams = Team.objects.all()
     return render(request, 'index.html', {'teams':teams}) """
-class TeamList(ListView):
+
+
+class TeamList(LoginRequiredMixin, ListView):
     model = Team
 
-    
+    def get_queryset(self):
+        return Team.objects.filter(user=self.request.user)
+
+    # limit to my own
+
+
 def detail(request, team_id):
     team = Team.objects.get(id=team_id)
-    return render(request, 'teams/team_detail.html', {'team':team})
-    
-class TeamCreate(CreateView):
-    model = Team
-    fields = '__all__'
-    pk_url_kwarg = 'team_id'
-    
-    
-class TeamDelete(DeleteView):
-    model = Team
-    pk_url_kwarg = 'team_id'
-    success_url = '/teams/'
-    
-    
-class TeamUpdate(UpdateView):
-    model = Team
-    fields = '__all__'
-    pk_url_kwarg = 'team_id'
-    template_name = './team_up_app/team_form.html' #teamplate name needs to be address! within template folder!
-    success_url = '/teams/'
-    
+    return render(request, "teams/team_detail.html", {"team": team})
 
-#auth
 
-class About(LoginView): #home 
-    template_name='about.html'
-    
+class TeamCreate(LoginRequiredMixin, CreateView):
+    model = Team
+    fields = ["name","top","mid","bot","support","jungle"]
+    pk_url_kwarg = "team_id"
+    def form_valid(self, form):
+        # this is a place where you can add things to the form once it was submitted to the
+        # server
+
+        # assign the logged in user to the form (instance)that was submitted to the server
+        form.instance.user = self.request.user # self.request.user is the logged in user! 
+        # let the form add the new to the database now!
+        return super().form_valid(form)
+"""   def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.send_email()
+        return super().form_valid(form)
+
+"""
+class TeamDelete(LoginRequiredMixin, DeleteView):
+    model = Team
+    pk_url_kwarg = "team_id"
+    success_url = "/teams/"
+
+
+class TeamUpdate(LoginRequiredMixin, UpdateView):
+    model = Team
+    fields = fields = ["name","top","mid","bot","support","jungle"]
+    pk_url_kwarg = "team_id"
+    template_name = "./team_up_app/team_form.html"  # teamplate name needs to be address! within template folder!
+    success_url = "/teams/"
+
+
+# auth
+
+
+class About(LoginView):  # home
+    template_name = "about.html"
+
 
 def signup(request):
-    error_message = ''
-    if request.method == 'POST':
+    error_message = ""
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
-        if form.is_valid:
-            user = form.save() #saves new user to database
-            login(request,user)
-            return redirect('team_list') #appears different bc user if/else
+        if form.is_valid():
+            user = form.save()  # saves new user to database
+            login(request, user)
+            return redirect("team-list")  # appears different bc user if/else
         else:
-            error_message = 'Please try again :('
+            error_message = "Please try again :("
     else:
-        form = UserCreationForm()  # Create an empty form for GET requests
+        form = UserCreationForm()
+        print("bug is here")
+        # Create an empty form for GET requests
 
-    context = {'form': form, 'error_message': error_message}
-    return render(request, 'signup.html', context)
+    context = {"form": form, "error_message": error_message}
+    print("line 86")
+    print(context)
+    return render(request, "signup.html", context)
